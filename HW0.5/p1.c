c772f318e22acdad32fa10f7e02fde0c48509af5
Takala <dylan91011028@gmail.com>
2023-03-01 22:05:23 +0800

    update

--- HW0.5/p1.c
+++ HW0.5/p1.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 int main() {
-    int t;
-    scanf("%d", &t);
-    for(long long i = 0, a = 0, b = 0; i < t; i++) {
+    long long t, a, b;
+    scanf("%ld", &t);
+    while(t--) {
         scanf("%ld %ld", &a, &b);
         printf("%ld\n", a + b);
     }
-
     return 0;
 }
