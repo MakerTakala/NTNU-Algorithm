354a0b1ac28cac498e5d8cab5a665b27bf09fe41
Takala <dylan91011028@gmail.com>
2023-03-20 13:44:56 +0800

    HW1 fin

--- HW1/p3.cpp
+++ HW1/p3.cpp
@@ -1,39 +1,84 @@
 #include <bits/stdc++.h>
-#define Block pair<int, int>
 #define x first
 #define y second
 using namespace std;
 
-vector<int> add(vector<int> a, vector<int> b) {
-    for(int i = 0; i < a.size(); i++) {
-        a[i] += b[i];
+int cur_l_block = 1;
+
+void one_black(vector<vector<int>> &puzzle, pair<int, int> black, pair<int, int> lu_pos, int size) {
+    if(size == 1) return;
+    size /= 2;
+    int mid_x = lu_pos.x + size, mid_y = lu_pos.y + size;
+
+    if(black.x >= mid_x || black.y >= mid_y) {
+        puzzle[mid_x - 1][mid_y - 1] = cur_l_block;
     }
-    return a;
+    if(black.x < mid_x || black.y >= mid_y) {
+        puzzle[mid_x][mid_y - 1] = cur_l_block;
+    }
+    if(black.x >= mid_x || black.y < mid_y) {
+        puzzle[mid_x - 1][mid_y] = cur_l_block;
+    }
+    if(black.x < mid_x || black.y < mid_y) {
+        puzzle[mid_x][mid_y] = cur_l_block;
+    }
+    cur_l_block++;
+
+    one_black(puzzle, black.x >= mid_x || black.y >= mid_y ? make_pair(mid_x - 1, mid_y - 1) : black, {lu_pos.x, lu_pos.y}, size);
+    one_black(puzzle, black.x < mid_x || black.y >= mid_y ? make_pair(mid_x, mid_y - 1) : black, {mid_x, lu_pos.y}, size);
+    one_black(puzzle, black.x >= mid_x || black.y < mid_y ? make_pair(mid_x - 1, mid_y) : black, {lu_pos.x, mid_y}, size);
+    one_black(puzzle, black.x < mid_x || black.y < mid_y ? make_pair(mid_x, mid_y) : black, {mid_x, mid_y}, size);
 }
 
-bool fail = false;
-
-vector<int> solver(vector<Block> blacks, int puzzle_size) {
-    if(puzzle_size == 1) return vector<int>(4, 0);
+void solver(vector<vector<int>> &puzzle, vector<pair<int, int>> blacks, pair<int, int> lu_pos, int size) {
     if(blacks.size() == 1) {
-        
+        one_black(puzzle, blacks.front(), lu_pos, size);
+        return;
     }
 
+    vector<pair<int, int>> lu_blacks, ru_blacks, lb_blacks, rb_blacks;
+    size /= 2;
+    int mid_x = lu_pos.x + size, mid_y = lu_pos.y + size;
+    for(auto black : blacks) {
+        if(black.x < mid_x && black.y < mid_y) {
+            lu_blacks.push_back(black);
+        }
+        else if(black.x >= mid_x && black.y < mid_y) {
+            ru_blacks.push_back(black);
+        }
+        else if(black.x < mid_x && black.y >= mid_y) {
+            lb_blacks.push_back(black);
+        }
+        else if(black.x >= mid_x && black.y >= mid_y) {
+            rb_blacks.push_back(black);
+        }
+    }
+    solver(puzzle, lu_blacks, {lu_pos.x, lu_pos.y}, size);
+    solver(puzzle, ru_blacks, {mid_x, lu_pos.y}, size);
+    solver(puzzle, lb_blacks, {lu_pos.x, mid_y}, size); 
+    solver(puzzle, rb_blacks, {mid_x, mid_y}, size);
 }
+
 
 int main() {
     int n, l;
     cin>>n>>l;
-    if(n % 3 != 1) cout<<"Can not covered."<<endl;
 
-    vector<Block> blacks(l);
-    vector<int> ans(4, 0);
+    vector<vector<int>> puzzle(l, vector<int>(l, 0));
+    vector<pair<int, int>> blacks(n);
 
-    for(Block &black : blacks) {
-        cin>>black.x>>black.y;
+    for(int i = 0, x, y; i < n; i++) {
+        cin>>blacks[i].x>>blacks[i].y;
     }
 
+    solver(puzzle, blacks, {0, 0}, l);
 
+    for(auto y : puzzle) {
+        for(auto x : y) {
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
