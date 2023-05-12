6d97f335078d03257f62bf16dd3dcc4fc6a36367
Takala <dylan91011028@gmail.com>
2023-05-13 00:58:55 +0800

    HW3 fin

--- HW3/p4.cpp
+++ HW3/p4.cpp
@@ -7,11 +7,15 @@ int main() {
 
     int n, m;
     cin>>n>>m;
+    vector<int> input(m);
     vector<int> pos(n + 1, -1);
+    vector<int> group(n + 1, 0);
+
 
     int size = -1;
     for(int i = 0, x, pre = INT_MAX; i < m; i++) {
         cin>>x;
+        input[i] = x;
         if(x < pre) {
             size++;
         }
@@ -32,13 +36,20 @@ int main() {
             if(visited[pos[i]]) {
                 ans++;
                 fill(visited.begin(), visited.end(), 0);
-                visited[pos[i]] = 1;
             }
-            else {
-                visited[pos[i]] = 1;
-            }
+            visited[pos[i]] = 1;
+            group[i] = ans;
+        }
+        // for(auto x : pos)
+        //     cout<<x<<" ";
+        // cout<<endl;
+        // for(auto x : group)
+        //     cout<<x<<" ";
+        if(group[input[m - 1]] == group[input[m - 2]]) {
+            ans++;
         }
         cout<<ans<<endl;
+
     }
 
     return 0;
