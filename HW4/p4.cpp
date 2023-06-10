5966f32b8bf0a98ac2e909f4e7ccca979a940836
Takala <dylan91011028@gmail.com>
2023-06-10 16:51:20 +0800

    update

--- HW4/p4.cpp
+++ HW4/p4.cpp
@@ -1,88 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int S, T, g_size;
+#define INF INT_MAX
+#define MAX_SIZE 205
 
-vector<long long int> BFS(vector<vector<pair<int, long long int>>> &graph) {
-    vector<int> visited(g_size, 0);
-    queue<vector<long long int>> q;
-    q.push(vector<long long int>{0});
+int n, S, T, g_size;
 
+vector<int> pre(MAX_SIZE);
+vector<int> flow(MAX_SIZE, 0);
+
+int BFS(vector<vector<int>> &graph) {
+    fill(pre.begin(), pre.begin() + g_size, -1);
+    fill(flow.begin(), flow.begin() + g_size, 0);
+    queue<int> q;
+    flow[S] = INF;
+    pre[S] = 0;
+    q.push(S);
+    
     while(!q.empty()) {
-        vector<long long int> top = q.front(); q.pop();
-        int u = top.back();
-        visited[u] = 1;
-        for(auto [v, w] : graph[u]) {
-            if(visited[v] == 1) continue;
-            top.push_back(v);
-            if(v == T) return top;
-            q.push(top);
-            top.pop_back();
+        int u = q.front(); q.pop();
+        if(u == T) break;
+        for(int i = 1; i < g_size; i++) {
+            if(graph[u][i] > 0 && pre[i] == -1) {
+                pre[i] = u;
+                flow[i] = min(flow[u], graph[u][i]);
+                q.push(i);
+            }
         }
     }
-    return vector<long long int>();
+
+    if(pre[T] == -1) return -1;
+    return flow[T];
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n;
     cin>>n;
 
     S = 0; T = 2 * n + 1; g_size = 2 * n + 2;
-    vector<vector<pair<int, long long int>>> graph(g_size);
+
+    vector<vector<int>> graph(g_size, vector<int> (g_size, 0));
     
-    long long int sum = 0, x;
+    int sum = 0, x;
 
     for(int i = 0; i < n; i++) {
         cin>>x;
-        graph[S].push_back({i + 1, x});
+        graph[S][i + 1] = x;
+        graph[i + 1][n + 1 + i] = x;
     }
     for(int i = 0; i < n; i++) {
         cin>>x;
+        graph[n + 1 + i][T] = x;
         sum += x;
-        graph[n + 1 + i].push_back({T, x});
     }
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             cin>>x;
-            if(x == 0) continue;
-            graph[i + 1].push_back({n + 1 + j, x});   
-        }
-        cout<<i;
-        cout<<"OK"<<endl;
-    }
-    for(int i = 0; i < n; i++) {
-        graph[i + 1].push_back({n + 1 + i, LLONG_MAX});
-    }
-
-
-    while(1) {
-        vector<long long int> argu_road = BFS(graph);
-        
-        if(argu_road.size() == 0) break;
-        
-        long long int max_flow = LLONG_MAX;
-        for(int i = 0; i < argu_road.size() - 1; i++) {
-            auto it = find_if(graph[argu_road[i]].begin(), graph[argu_road[i]].end(), [&](pair<int, long long int> value) { return value.first == argu_road[i + 1]; });
-            max_flow = min(max_flow, it->second);
-        }
-
-        sum -= max_flow;
-
-        for(int i = 0; i < argu_road.size() - 1; i++) {
-            auto it1 = find_if(graph[argu_road[i]].begin(), graph[argu_road[i]].end(), [&](pair<int, long long int> value) { return value.first == argu_road[i + 1]; });
-            it1->second -= max_flow;
-
-            auto it2 = find_if(graph[argu_road[i + 1]].begin(), graph[argu_road[i + 1]].end(), [&](pair<int, long long int> value) { return value.first == argu_road[i]; });
-            if(it2 == graph[argu_road[i + 1]].end()) {
-                graph[argu_road[i + 1]].push_back({argu_road[i], max_flow});
-            }
-            else {
-                it2->second += max_flow;
+            if(x != 0) {
+                graph[i + 1][n + 1 + j] = x;
             }
         }
+    }
+
+    int add_flow = 0;
+    while(add_flow = BFS(graph) != -1) {
+        int node = T;
+        while(node != S) {
+            int last = pre[node];
+            graph[node][last] += add_flow;
+            graph[last][node] -= add_flow;
+            node = last;
+        }
+        sum -= add_flow;
     }
 
     cout<<sum<<endl;
